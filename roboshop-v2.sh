d2d0cf07921d8bd364ecf54d79d2640098122b82
Vk167 <viijaykumarr07@gmail.com>
2026-06-11 07:49:45 +0530

    roboshop-v2

--- roboshop-v2.sh
+++ roboshop-v2.sh
@@ -22,12 +22,9 @@ fi
 
 get_instance_id(){
     name=$1
-     (aws ec2 describe-instances --filters "Name=tag:Name,Values=roboshop-$name" "Name=instance-state-name,Values=running" --query "Reservations[0].Instances[0].InstanceId" --output text)
-    # ( aws ec2 describe-instances \
-    # --filters "Name=tag:Name,Values=roboshop-$name" "Name=instance-state-name,Values=running" \
-    # --query "Reservations[*].Instances[*].InstanceId" \
-    # --output text
-    # )
+     ( aws ec2 describe-instances --filters "Name=tag:Name,Values=roboshop-$name" "Name=instance-state-name,Values=running" --query "Reservations[0].Instances[0].InstanceId" --output text 
+     )
+    
 }
 
 for instance in $@
