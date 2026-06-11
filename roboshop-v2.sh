8b985cbd5b3751b65499ff2196b96311f91ae6d2
Vk167 <viijaykumarr07@gmail.com>
2026-06-11 08:19:03 +0530

    roboshop-v2

--- roboshop-v2.sh
+++ roboshop-v2.sh
@@ -122,6 +122,12 @@ do
                         ]
                     }
                 '
-                echo "Updated Route53 record for existing: $instance"
+            echo "Updated Route53 record for existing: $instance"
+        else
+            if [ "$INSTANCE_ID" == "None" ]; then
+                echo "$instance already destroyed, nothing to do..."
+            else
+                aws ec2 terminate-instances --instance-ids $INSTANCE_ID
+                echo "Terminating instance: $instance
     fi
 done
