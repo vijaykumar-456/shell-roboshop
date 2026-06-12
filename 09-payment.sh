5d9fbe4a0108c0c1f03d6cf85611a4f28b58be6d
Vk167 <viijaykumarr07@gmail.com>
2026-06-12 08:47:56 +0530

    roboshop-v2

---
+++ 09-payment.sh
@@ -0,0 +1,77 @@
+#!bin/bash
+
+LOG_FOLDER='/var/log/roboshop'
+
+sudo mkdir -p $LOG_FOLDER
+
+sudo chown -R ec2-user:ec2-user $LOG_FOLDER
+
+sudo chmod -R 755 $LOG_FOLDER
+
+LOG_FILE="/$LOG_FOLDER/$0.log"
+
+SCRIPT_DIR=$PWD
+
+MYSQL_HOST=mysql.learndevopskills.shop
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S" )
+
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ]; then
+    echo -e "$TIMESTAMP [ERROR] $R Please access with admin user $N" | tee -a $LOG_FILE
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$TIMESTAMP [ERROR] $2 ... $R FAILURE $N" | tee -a $LOG_FILE
+    else
+        echo -e "$TIMESTAMP [INFO] $2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf install python3 gcc python3-devel -y &>>LOG_FILE
+VALIDATE $? "Installing maven"
+
+id roboshop &>> $LOG_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating roboshop system user"
+else
+    echo -e "$R User Already present with this name $N ... $Y SKIPPING $N"
+fi
+
+rm -rf /app
+VALIDATE $? "Removing Existing app/code"
+
+rm -rf /tmp/payment.zip
+VALIDATE $? "Removing Exisiting payment"
+
+mkdir -p /app &>> $LOG_FILE
+VALIDATE $? "Creating app folder for code"
+
+curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip  &>>$LOG_FILE
+cd /app
+unzip /tmp/payment.zip &>>$LOG_FILE
+VALIDATE $? "Unzipping the payment code"
+
+pip3 install -r requirement.txt &>> $LOG_FILE
+VALIDATE $? "Installing python dependencies"
+
+
+cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service
+VALIDATE $? "Creating systemctl service"
+
+systemctl enable payment
+systemctl restart payment
+VALIDATE $? "Enabling and restarting the payment"
+
+
+
+
